fc6733e37a9d93a1887f1db6e0588a43a77ba8d1
Mark Burggraf <markb@mantisbible.com>
2021-08-10 16:03:36 -0700

    begin move to streaming local files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Papa from 'papaparse'
+
+import { analyzeRow, analyzeRowResults, detectType, determineWinner } from './importHelpers';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,191 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public importSpec = {  
+    sourceType: 'csv',
+    sourceURL: '',
+    delimiter: ',',
+    enclosedBy: '',
+    fieldNames: '',
+    headerLine: '1',
+    domain: '',
+    port: '',
+    database: '',
+    user: '',
+    password: '',
+    destinationTable: '',
+    truncate: '0',
+    isReady: false,
+    ddl: '',
+    DDL: '',
+    jobId: '',
+    jobName: '',
+    status: 'ready',
+    RESULTS: [],
+    SUPABASE_URL: '',
+    SUPABASE_KEY: ''
+  };
+
+  constructor() {
+    console.log(Papa);
+  }
+
+  async start() {
+    await this.analyzeFile();
+    // console.log('this.importSpec.destinationTable', this.importSpec.destinationTable);
+    // console.log('this.importSPec.ddl', this.importSpec.ddl);
+    console.log('this.importSpec', this.importSpec);
+    // console.log('this.DDL', this.DDL);
+    // this.DDL = `CREATE TABLE ${this.importSpec.destinationTable} ` + this.DDL;
+    // console.log('this.DDL', this.DDL);
+  }
+
+  analyzeFile = async () => {
+    const fieldsHash = {};
+    const importSpec: any = this.importSpec;
+    importSpec.status = 'analyzing';
+    Papa.LocalChunkSize = 1024 * 1024 * 10;	// 10 MB
+    Papa.RemoteChunkSize = 1024 * 1024 * 10;	// 5 MB
+    const fileElement: any = document.getElementById('files');
+    const file = fileElement.files[0];
+    let rowCount = 0;
+    let fieldNameArr = [];
+    let fieldsArray = [];
+    await Papa.parse(file, {
+      download: false, // true,
+      // quoteChar: importSpec.enclosedBy,
+      header: true, //(importSpec.headerLine === '1'),
+      skipEmptyLines: true,
+      // LocalChunkSize: 1024 * 1024 * 0.25, // 100 MB
+      // RemoteChunkSize: 1024 * 1024 * 100, // 100 MB
+      // newline: '\r\n',
+      // worker: true,
+      chunk: function(results, parser) {
+        if (importSpec.abort) parser.abort();
+        console.log("Row data:", results.data);
+        console.log("Row errors:", results.errors);
+        console.log('Meta', results.meta);
+        if (!fieldNameArr.length) fieldNameArr = results.meta.fields;
+        // console.log('parser', parser);
+        results.data.map((row) => {
+          if (rowCount > 0 || (importSpec.headerLine === '0')) analyzeRow(fieldsHash, row);
+          rowCount++;
+        });
+      },
+      complete: function() {
+        console.log('complete!');
+        console.log('fieldsHash', fieldsHash);
+        console.log('fieldNames is now', importSpec.fieldNames);
+        const fieldsArray = analyzeRowResults(fieldsHash);
+        console.log('fieldsArray', fieldsArray);
+        
+        // let DDL = `create table "${getFileName(importSpec.sourceURL, true)}" (`;
+        
+        importSpec.ddl = [];
+        const assignedFieldNames = [];
+        let DDL = `(`;
+        for (let x = 0; x < fieldsArray.length; x++) {
+          let fieldName = (fieldNameArr[x] || 'field').trim();
+          if (assignedFieldNames.indexOf(fieldName) > -1) {
+            let suffix = 1;
+            while (assignedFieldNames.indexOf(fieldName + suffix) > -1) {
+              suffix++;
+            }
+            // console.log('adding suffix', suffix, 'to fieldName', fieldName);
+            fieldName += suffix;
+          }
+          DDL += `"${fieldName}" ${fieldsArray[x].type.toUpperCase()}`;
+          assignedFieldNames.push(fieldName.trim());
+          importSpec.ddl.push([`${fieldName}`,`${fieldsArray[x].type.toUpperCase()}`]);
+          if (x < fieldsArray.length - 1) DDL += `, `;
+        }
+        DDL += `)`;        
+        console.log('DDL', DDL);
+        importSpec.DDL = `CREATE TABLE "${importSpec.destinationTable}" ${DDL}`;
+        importSpec.fieldNames = assignedFieldNames.join(',');
+        importSpec.status = 'analyzed';
+        
+
+      }
+      /*
+      complete: function(results, file) {
+        console.log('results', results);
+        const errors = results.errors;
+        let rowCount = 0;
+        results.data.map((row) => {
+          if (rowCount > 0 || (importSpec.headerLine === '0')) analyzeRow(fieldsHash, row);
+          rowCount++;
+        });
+  
+        const fieldsArray = analyzeRowResults(fieldsHash);
+  
+        if (results?.errors?.length) {
+          console.error('Parse Errors', results.errors);
+        } 
+        const delimiter = results?.meta?.delimiter;
+        const linebreak = results?.meta?.linebreak;
+        if (linebreak === '\r') {
+          errors.unshift('files with a line terminator of \\r are not currently supported');
+        }
+        rowCount = results?.data?.length;
+        importSpec.RESULTS = results;
+        
+        console.log('delimiter', delimiter.replace('\t','\\t').replace('\n','\\n').replace('\r','\\r'));
+        console.log('linebreak', linebreak.replace('\t','\\t').replace('\n','\\n').replace('\r','\\r'));
+        console.log('importSpec.headerLine', importSpec.headerLine);
+        if (importSpec.headerLine === '1') {
+          console.log('headers', results?.data[0]?.join(','));
+        } else {
+          console.log('no headers');
+        }
+        console.log('rowCount', rowCount);
+        importSpec.delimiter = delimiter.replace('\t','\\t').replace('\n','\\n').replace('\r','\\r');
+        console.log('headerline looks like:', results?.data[0]);
+        if (importSpec.headerLine === '1') {
+          importSpec.fieldNames = results?.data[0]?.join(',');
+          console.log('fieldNames is now', importSpec.fieldNames);
+        } else {
+          importSpec.fieldNames = '';
+          for (let i = 0; i < fieldsArray.length; i++) {
+            importSpec.fieldNames += `,field${i+1}`;
+          }
+          importSpec.fieldNames = importSpec.fieldNames.substring(1);
+        }
+        console.log('fieldNames is now', importSpec.fieldNames);
+        const fieldNameArr = importSpec.fieldNames.split(',');
+        console.log('fieldNames is now', importSpec.fieldNames);
+        
+        // let DDL = `create table "${getFileName(importSpec.sourceURL, true)}" (`;
+        importSpec.ddl = [];
+        const assignedFieldNames = [];
+        let DDL = `(`;
+        for (let x = 0; x < fieldsArray.length; x++) {
+          let fieldName = (fieldNameArr[x] || 'field').trim();
+          if (assignedFieldNames.indexOf(fieldName) > -1) {
+            let suffix = 1;
+            while (assignedFieldNames.indexOf(fieldName + suffix) > -1) {
+              suffix++;
+            }
+            // console.log('adding suffix', suffix, 'to fieldName', fieldName);
+            fieldName += suffix;
+          }
+          DDL += `"${fieldName}" ${fieldsArray[x].type.toUpperCase()}`;
+          assignedFieldNames.push(fieldName.trim());
+          importSpec.ddl.push([`${fieldName}`,`${fieldsArray[x].type.toUpperCase()}`]);
+          if (x < fieldsArray.length - 1) DDL += `, `;
+        }
+        DDL += `)`;        
+        console.log('DDL', DDL);
+        importSpec.DDL = `CREATE TABLE "${importSpec.destinationTable}" ${DDL}`;
+        importSpec.fieldNames = assignedFieldNames.join(',');
+        importSpec.status = 'analyzed';
+      }
+      */
+    });
+  }
+  
+  importCSV = async (importSpec) => {
+
+  }
 
 }
---
+++ src/app/home/importHelpers.js
@@ -0,0 +1,163 @@
+// write a function to test whether a string is a valid postgres timestamptz
+export const isPostgresTimestamp = (str) => {
+    if (typeof str !== 'string') return false;
+    var match = str.match(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})\.\d+$/);
+    if (match) {
+      var year = match[1],
+          month = match[2],
+          day = match[3],
+          hour = match[4],
+          minute = match[5],
+          second = match[6]
+  
+      if (year.length === 4 && month.length === 2 && day.length === 2 && hour.length === 2 && minute.length === 2 && second.length === 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+  //write a function to test whether a string is a valid postgres datetime
+export const isPostgresDateTime = (str) => {
+    if (typeof str !== 'string') return false;
+    var match = str.match(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/);
+    if (match) {
+      var year = match[1],
+          month = match[2],
+          day = match[3],
+          hour = match[4],
+          minute = match[5],
+          second = match[6]
+      if (year.length === 4 && month.length === 2 && day.length === 2 && hour.length === 2 && minute.length === 2 && second.length === 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+  
+export const isPostgresDate = (str) =>{
+    if (typeof str !== 'string') return false;
+    var match = str.match(/^(\d{4})-(\d{2})-(\d{2})$/);
+    if (match) {
+      var year = match[1],
+          month = match[2],
+          day = match[3];
+      if (year.length === 4 && month.length === 2 && day.length === 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+  
+export const analyzeRow = (fieldsHash, row) =>{
+    for (let key in row) {
+      const value = row[key]
+      const field = fieldsHash[key] || (fieldsHash[key] = { typesFound: {}, sample: null, maxLength: 0, enabled: true })
+  
+      // Tally the presence of this field type
+      const type = detectType(value)
+      if (!field.typesFound[type]) field.typesFound[type] = 0
+      field.typesFound[type]++
+  
+      // Save a sample record if there isn't one already (earlier rows might have an empty value)
+      if (!field.sample && value) {
+        field.sample = value
+      }
+  
+      // Save the largest length
+      field.maxLength = Math.max(field.maxLength, value.length)
+    }
+  }
+  
+export const detectType = (sample) =>{
+    if (typeof sample !== 'string' || sample === '') {
+      return 'text'
+    } else if (isPostgresTimestamp(sample) && +sample >= 31536000) { 
+      return 'timestamp'
+    } else if (isPostgresDate(sample)) {
+      return 'date'
+    } else if (isPostgresDateTime(sample)) {
+      return 'datetime'
+    } else if (!isNaN(sample) && sample.includes('.')) {
+      return 'float'
+    } else if (sample === '1' || sample === '0' || ['true', 'false'].includes(sample.toLowerCase())) {
+      return 'boolean'
+    } else if (!isNaN(sample)) {
+        try {
+          // 4,294,967,295
+          // if (sample.length > 9 && parseInt(sample.substr(0,1),10) > 3) {
+          if (parseInt(sample, 10) > 4294967295) {
+            return 'bigint';
+          } else {
+            return 'integer';
+          }  
+        } catch (e) {
+            return 'bigint';
+        }
+    } else if (sample.length > 255) {
+      return 'text'
+    } else {
+      return 'text' // string
+    }
+  }
+  
+export const analyzeRowResults = (fieldsHash) => {
+    let fieldsArray = []
+    for (let key in fieldsHash) {
+      const field = fieldsHash[key]
+      // Determine which field type wins
+      field.type = determineWinner(field.typesFound)
+      field.machineName = key
+      // field.machineName = slug(key, {
+      //   replacement: '_',
+      //   lower: true
+      // })
+      field.sourceName = key
+      // If any null values encountered, set field nullable
+      if (field.typesFound['null']) {
+        field.nullable = true
+      }
+      fieldsArray.push(field)
+    }
+    return fieldsArray
+  }
+  
+  /**
+   *  Determine which type wins
+   *  - timestamp could be int
+   *  - integer could be float
+   *  - everything could be string
+   *  - if detect an int, don't check for timestamp anymore, only check for float or string
+   *  - maybe this optimization can come later...
+   */
+export const determineWinner = (fieldTypes) =>{
+    const keys = Object.keys(fieldTypes)
+  
+    if (keys.length === 1) {
+      return keys[0]
+    } else if (fieldTypes.text) {
+      return 'text'
+    } else if (fieldTypes.string) {
+      return 'string'
+    } else if (fieldTypes.float) {
+      return 'float'
+    } else if (fieldTypes.bigint) {
+        return 'bigint'
+    } else if (fieldTypes.integer) {
+      return 'integer'
+    } else if (fieldTypes.boolean) {
+        return 'boolean'
+    } else { // TODO: if keys.length > 1 then... what? always string? what about date + datetime?
+      console.log('undetermined field type');
+      console.log('keys', keys);
+      console.log('fieldTypes', fieldTypes);
+      return fieldTypes[0]
+    }
+  }
+  
+  
